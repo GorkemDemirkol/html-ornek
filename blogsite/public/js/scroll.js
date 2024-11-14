998f758614c33cc50e8b859b6eeb7c2a2bc3a6f6
GorkemDemirkol <s.gorkem.demirkol@hotmail.com>
2024-11-14 23:00:13 +0300

    Add files via upload

--- blogsite/public/js/scroll.js
+++ blogsite/public/js/scroll.js
@@ -6,7 +6,12 @@ const options={
 }
 const callBack=(entries)=>{
     entries.forEach((entry)=>{
-        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('active')
+        }
+        else{
+            entry.target.classList.remove('active')
+        }
     })
 }
 
